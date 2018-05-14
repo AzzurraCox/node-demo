3ecd49f394e2f8251900d612bb28aae02bd684e2
Unknown <rotiong97@gmail.com>
2018-05-14 21:06:49 +0800

    crawl

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const Xray = require('x-ray');
+x = Xray();
+
+x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html','table > tr',[
+    {
+        no:'td:nth-child(2) > font',
+        name:'td:nth-child(3) > font',
+        teacher:'td:nth-child(4) > font',
+    },
+]).write('./hello.json');
